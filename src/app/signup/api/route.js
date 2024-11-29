8064bb7cf096b26307f6b29fc627ccfee84c73fa
HabibFahim15 <habibfahim71@gmail.com>
2024-11-30 03:47:35 +0600

    dotenv created and hide the secret mongodb code

--- src/app/signup/api/route.js
+++ src/app/signup/api/route.js
@@ -1,4 +1,4 @@
-import { connectDB } from "@/lib/connectDB";
+import { connectDB } from "../../../lib/connectDB.js";
 
 export const POST = async (request) => {
 
--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -4,7 +4,7 @@ let db
 export const connectDB =async () => {
   if(db) return db;
   try {
-    const uri = `mongodb+srv://suitetrack:l2PnToJ8rwDAHxXo@cluster0.i5to1lc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    const uri = `mongodb+srv://${process.env.NEXT_PUBLIC_DB_USER}:${process.env.NEXT_PUBLIC_DB_PASS}@cluster0.i5to1lc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     const client = new MongoClient(uri, {
       serverApi:{
         version: ServerApiVersion.v1,
