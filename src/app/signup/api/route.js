9880face915f1eaa95d7250065bd7ee679225cd2
HabibFahim15 <habibfahim71@gmail.com>
2024-11-30 23:02:22 +0600

    heje biji

--- src/app/signup/api/route.js
+++ src/app/signup/api/route.js
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server.js";
 import { connectDB } from "../../../lib/connectDB.js";
 import bcrypt from 'bcrypt'
 export const POST = async (request) => {
@@ -8,15 +9,15 @@ export const POST = async (request) => {
     const userCollection = db.collection('users');
     const exist = await userCollection.findOne({email: newUser.email});
     if(exist){
-      return Response.json({messege: "User Already Exist"}, {status: 400});
+      return NextResponse.json({messege: "User Already Exist"}, {status: 400});
     }
     const hashedPassword = await bcrypt.hash(newUser.password, 10);
     const resp = await userCollection.insertOne({...newUser, password: hashedPassword});
 
 
-    return  Response.json({messege: "User Created"}, {status: 200});
+    return  NextResponse.json({messege: "User Created"}, {status: 200});
   } catch (error) {
-    return  Response.json({messege: "Something went wrong",error}, {status: 500});
+    return  NextResponse.json({messege: "Something went wrong",error}, {status: 500});
   }
 
 }
