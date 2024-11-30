4a32695f7cbad4a6262da17b15dca84c4f2c5f11
HabibFahim15 <habibfahim71@gmail.com>
2024-11-30 23:18:51 +0600

    s

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div >
-      
+      {/* a */}
       <div className='container mx-auto'>
         <Hero />
       </div>
