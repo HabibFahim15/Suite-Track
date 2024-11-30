35a5136f459b328b0b7f5078f204a81c27a22f3d
HabibFahim15 <habibfahim71@gmail.com>
2024-11-30 19:23:26 +0600

    redeploy

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -14,7 +14,7 @@ const signUp = () => {
       password : event.target.password.value
     }
     console.log(newUser);
-    const resp = await fetch("http://localhost:3000/signup/api", {
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/signup/api`, {
       method : "POST",
       body: JSON.stringify(newUser),
       headers : {
